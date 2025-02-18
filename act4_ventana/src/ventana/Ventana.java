eecd450d6c19e4ec32f2da5bee9b842d03914ca8
Ricardog221 <ricardog_21@alu.uabcs.mx>
2025-02-18 09:54:02 -0700

    Avance de pantalla de registro

--- act4_ventana/src/ventana/Ventana.java
+++ act4_ventana/src/ventana/Ventana.java
@@ -4,12 +4,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
@@ -33,7 +38,8 @@ public class Ventana extends JFrame{
 		this.setMinimumSize(new Dimension(400, 400));
 		this.setMaximumSize(new Dimension(600, 600));
 		
-		this.add(login());
+//		this.add(login());
+		this.add(registro());
 		this.repaint();
 	}
 	
@@ -88,8 +94,105 @@ public class Ventana extends JFrame{
 		
 		panelLogin.revalidate();
 		
-		
-		
 		return panelLogin;			
+	}
+	
+	public JPanel registro() {
+		JPanel panelRegistro = new JPanel();
+		
+		panelRegistro.setLayout(null);
+		panelRegistro.setSize(500, 500);
+		
+		JLabel etiqueta = new JLabel("Registro");
+		etiqueta.setFont(fuenteGrande);
+		etiqueta.setBounds(172, 30, 140, 30);
+		etiqueta.setOpaque(true);
+		etiqueta.setBackground(Color.ORANGE);		
+		etiqueta.setHorizontalAlignment(JLabel.CENTER);
+		
+		panelRegistro.add(etiqueta);
+		
+		//Nombre de usuario
+		JLabel textoUsuario = new JLabel("Nombre de usuario");
+		textoUsuario.setFont(fuenteMediana);
+		textoUsuario.setBounds(140, 80, 120, 20);
+		panelRegistro.add(textoUsuario);
+		
+		JTextField campoUsuario = new JTextField();
+		campoUsuario.setBounds(140, 100, 204, 20);
+		panelRegistro.add(campoUsuario);
+		
+		
+		//Biografia
+		JLabel textoBio = new JLabel("Biografia");
+		textoBio.setFont(fuenteMediana);
+		textoBio.setBounds(140, 120, 120, 20);
+		panelRegistro.add(textoBio);
+		
+		JTextArea campoBio = new JTextArea();
+		campoBio.setBounds(140, 140, 204, 80);
+		campoBio.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		panelRegistro.add(campoBio);
+		
+		
+		//Preferencias
+		JLabel textoPreferencias= new JLabel("Preferencias");
+		textoPreferencias.setFont(fuenteMediana);
+		textoPreferencias.setBounds(140, 220, 120, 20);
+		panelRegistro.add(textoPreferencias);
+					
+		JCheckBox checkBoxDulce = new JCheckBox("Dulce");
+		checkBoxDulce.setFont(fuenteMediana);
+		checkBoxDulce.setBounds(140, 240, 80, 20);
+		panelRegistro.add(checkBoxDulce);
+		
+		JCheckBox checkBoxSalado = new JCheckBox("Salado");
+		checkBoxSalado.setFont(fuenteMediana);
+		checkBoxSalado.setBounds(220, 240, 80, 20);
+		panelRegistro.add(checkBoxSalado);
+		
+		JCheckBox checkBoxSaludable = new JCheckBox("Saludable");
+		checkBoxSaludable.setFont(fuenteMediana);
+		checkBoxSaludable.setBounds(300, 240, 100, 20);
+		panelRegistro.add(checkBoxSaludable);
+		
+		
+		//Terminos
+		JLabel textoTerminos = new JLabel("Terminos");
+		textoTerminos.setFont(fuenteMediana);
+		textoTerminos.setBounds(140, 260, 120, 20);
+		panelRegistro.add(textoTerminos);
+		
+		JRadioButton botonAceptar = new JRadioButton("Acepto los términos");
+		botonAceptar.setFont(fuenteChica);
+		botonAceptar.setBounds(140, 280, 120, 20);
+		panelRegistro.add(botonAceptar);
+		
+		JRadioButton botonRechazar = new JRadioButton("No acepto los términos");
+		botonRechazar.setFont(fuenteChica);
+		botonRechazar.setBounds(260, 280, 130, 20);
+		panelRegistro.add(botonRechazar);
+		
+		ButtonGroup opcionesTerminos = new ButtonGroup();
+		opcionesTerminos.add(botonAceptar);
+		opcionesTerminos.add(botonRechazar);
+		
+		
+		//Elegir colonia
+		String[] coloniasDataset = {"Ayuntamiento", "Balandra", "Calafia", "Diana Laura", "El Progreso"};
+		
+		JComboBox<String> elegirColonia = new JComboBox<String>(coloniasDataset);
+		elegirColonia.setBounds(140, 300, 120, 30);
+		panelRegistro.add(elegirColonia);
+		
+		//Boton Crear Cuenta
+		JButton botonRegistro = new JButton("Crear Cuenta");
+		botonRegistro.setFont(fuenteMediana);
+		botonRegistro.setBounds(182, 350, 120, 30);
+		panelRegistro.add(botonRegistro);
+		
+		panelRegistro.revalidate();
+		
+		return panelRegistro;		
 	}
 }
