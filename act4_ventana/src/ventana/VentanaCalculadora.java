3fe8bfc1f379f0ee95d28812a8866ce7e9677cdf
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-02-26 08:16:17 -0700

    Unidad 2 Ejercicio 25

--- act4_ventana/src/ventana/VentanaCalculadora.java
+++ act4_ventana/src/ventana/VentanaCalculadora.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -33,18 +36,35 @@ public class VentanaCalculadora extends JFrame{
 		JPanel panelCalculadora = new JPanel();
 		panelCalculadora.setBackground(Color.BLACK);
 		panelCalculadora.setBorder(new EmptyBorder(10, 10, 10, 10));
-		panelCalculadora.setLayout(new BorderLayout(10, 10));
+		panelCalculadora.setLayout(new GridBagLayout());
+		GridBagConstraints reglas = new GridBagConstraints();
+		
 		
 		JLabel areaResultados = new JLabel("300.00");
 		areaResultados.setFont(fuente);
 		areaResultados.setOpaque(true);
-		panelCalculadora.add(areaResultados, BorderLayout.NORTH);
+		//El componente se encuentra en la celda (0, 0)
+		reglas.gridx = 0;
+		reglas.gridy = 0;
+		//El componente solo ocupa una celda de espacio
+		reglas.gridheight = 1;
+		reglas.gridwidth = 1; 
+		//Dimensiones de la celda
+		reglas.weightx = 1.0; //Ancho, es su valor maximo
+		reglas.weighty = 0.5; //Altura, es la mitad de su valor maximo
+		//Ambos componentes ocupan todo el espacio posible
+		reglas.fill = GridBagConstraints.BOTH;
+		//Espacio entre la etiqueta y los botones
+		reglas.insets = new Insets(0, 0, 10, 0);
+		
+		panelCalculadora.add(areaResultados, reglas);
+		reglas.insets = new Insets(0, 0, 0, 0); //Reiniciar los insets
 		
 		JPanel panelBotones = new JPanel();
 		panelBotones.setBackground(Color.BLACK);
 		panelBotones.setLayout(new GridLayout(5, 4, 10, 10));
 		
-		String[] textoBotones = { "CE", " ", " ", " ",
+		String[] textoBotones = { "MC", "M+", "+/-", "CE",
 									"7", "8", "9", "/",
 									"4", "5", "6", "*",
 									"1", "2", "3", "-",
@@ -53,7 +73,10 @@ public class VentanaCalculadora extends JFrame{
 			JButton boton = new JButton(textoBotones[i]);
 			boton.setSize(new Dimension(60, 60));
 			boton.setFont(fuente);
-			if(textoBotones[i].equals("CE") || textoBotones[i].equals(" ")) {
+			if(textoBotones[i].equals("MC") || textoBotones[i].equals("M+")) {
+				boton.setBackground(Color.RED);
+			}
+			else if(textoBotones[i].equals("CE") || textoBotones[i].equals("+/-")) {
 				boton.setBackground(Color.GRAY);
 			}
 			else if(textoBotones[i].equals("/") || textoBotones[i].equals("*") || textoBotones[i].equals("-") || textoBotones[i].equals("+") || textoBotones[i].equals("=")) {
@@ -65,8 +88,14 @@ public class VentanaCalculadora extends JFrame{
 			boton.setForeground(Color.WHITE);
 			panelBotones.add(boton);
 		}
-		
-		panelCalculadora.add(panelBotones, BorderLayout.CENTER);
+		reglas.gridx = 0;
+		reglas.gridy = 1;
+		reglas.gridheight = 1;
+		reglas.gridwidth = 1;
+		reglas.weighty = 1;
+		reglas.fill = GridBagConstraints.BOTH;
+
+		panelCalculadora.add(panelBotones, reglas);
 		
 		return panelCalculadora;
 	}
