1debe1fab6c8900e06614f03e3770ce9a9f0ea73
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-03-10 17:00:30 -0700

    Unidad 2 Ejercicio 33

--- act4_ventana/src/ventana/VentanaGraphics.java
+++ act4_ventana/src/ventana/VentanaGraphics.java
@@ -9,6 +9,9 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.LinearGradientPaint;
 import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -41,7 +44,8 @@ public class VentanaGraphics extends JFrame{
 		
 		Graphics2D g2d = (Graphics2D) g;
 //		pintarCasa(g2d);
-		pintarSMB3(g2d);
+//		pintarSMB3(g2d);
+		pintarSMW(g2d);
 	}
 	
 	public void pintarCasa(Graphics2D g2d) {
@@ -225,10 +229,10 @@ public class VentanaGraphics extends JFrame{
 		g2d.setPaint(null);
 		
 		//Bloques
-		this.drawBloque(g2d, 40, 300, 55);
-		this.drawBloque(g2d, 94, 300, 55);
+		this.drawBloque(g2d, 40, 300, 55, new Color(192, 147, 7), new Color(145, 98, 6));
+		this.drawBloque(g2d, 94, 300, 55, new Color(192, 147, 7), new Color(145, 98, 6));
 		//Bloque ?
-		this.drawBloque(g2d, 700, 300, 55);
+		this.drawBloque(g2d, 700, 300, 55, new Color(192, 147, 7), new Color(145, 98, 6));
 		g2d.setColor(Color.WHITE);
 		g2d.setFont(fuente);
 		g2d.drawString("?", 714, 343);
@@ -273,15 +277,124 @@ public class VentanaGraphics extends JFrame{
 		g2d.drawOval(x, y, radio, radio);
 	}
 
-	private void drawBloque(Graphics2D g2d, int x, int y, int size) {
-		g2d.setColor(new Color(192, 147, 7));
+	private void drawBloque(Graphics2D g2d, int x, int y, int size, Color color, Color colorSombreado) {
+		g2d.setColor(color);
 		g2d.setStroke(new BasicStroke(4));
 		g2d.fillRoundRect(x, y, size, size, 20, 20);
 		
 		float[] fraccionesBloque = {0.20f, 1.0f};
-		Color[] coloresBloque = {new Color(192, 147, 7), new Color(145, 98, 6)};
+		Color[] coloresBloque = {color, colorSombreado};
 		LinearGradientPaint gradienteBloque = new LinearGradientPaint(x, y, x+size, y+size, fraccionesBloque, coloresBloque);
 		g2d.setPaint(gradienteBloque);
 		g2d.drawRoundRect(x, y, size, size, 20, 20);
+	}
+	
+	private void pintarSMW(Graphics2D g2d) {
+		
+		GradientPaint gradienteCielo = new GradientPaint(0, 0, new Color(6, 101, 192), 0, 800, new Color(90, 174, 255));
+		g2d.setPaint(gradienteCielo);
+		g2d.fillRect(0, 0, 1000, 800);
+		
+		//Nubes traseras
+		this.drawNube(g2d, 300, 450);
+		this.drawNube(g2d, 150, 500);
+		this.drawNube(g2d, 330, 550);
+		this.drawNube(g2d, 650, 250);
+		this.drawNube(g2d, 600, 200);
+
+		
+		//Montañas traseras
+		Color color3 = new Color(200, 232, 230);
+		Color color4 = new Color(121, 182, 217);
+		this.drawFondo(g2d, 50, 200, 200, 600, color3, color4);
+		this.drawFondo(g2d, 400, 200, 180, 600, color3, color4);
+		this.drawFondo(g2d, 580, 150, 180, 650, color3, color4);
+		g2d.setColor(Color.WHITE);
+		g2d.fillOval(75, 430, 25, 50);
+		g2d.fillOval(155, 370, 25, 50);
+		g2d.fillOval(155, 260, 25, 50);
+		g2d.fillOval(450, 300, 25, 50);
+		g2d.fillOval(450, 400, 25, 50);
+		g2d.fillOval(510, 240, 25, 50);
+		g2d.fillOval(610, 240, 25, 50);
+		g2d.fillOval(700, 200, 25, 50);
+		
+		//Nube en medio
+		this.drawNube(g2d, -50, 320);
+		this.drawNube(g2d, 250, 350);
+		this.drawNube(g2d, 480, 400);
+		this.drawNube(g2d, 670, 470);
+		this.drawNube(g2d, 700, 550);
+		//Montañas enfrente
+		Color color1 = new Color(118, 178, 217);
+		Color color2 = new Color(21, 88, 117);
+		this.drawFondo(g2d, -50, 500, 200, 220, color1, color2);
+		this.drawFondo(g2d, 400, 500, 200, 220, color1, color2);
+		this.drawFondo(g2d, 610, 300, 200, 500, color1, color2);
+		this.drawFondo(g2d, 900, 400, 200, 500, color1, color2);
+		g2d.setColor(Color.WHITE);
+		g2d.fillOval(650, 350, 25, 50);
+		g2d.fillOval(750, 500, 25, 50);
+		this.drawTuberiaAzul(g2d, 470, 450);
+		this.drawTuberiaAzul(g2d, 910, 500);
+		
+		this.drawBloque(g2d, 850, 550, 50, new Color(177, 180, 185), new Color(124, 121, 124));
+		this.drawBloque(g2d, 850, 495, 50, new Color(177, 180, 185), new Color(124, 121, 124));
+		this.drawBloque(g2d, 850, 440, 50, new Color(177, 180, 185), new Color(124, 121, 124));
+		this.drawBloque(g2d, 850, 385, 50, new Color(177, 180, 185), new Color(124, 121, 124));
+		
+		this.drawBloque(g2d, 905, 385, 50, new Color(246, 216, 46), new Color(223, 162, 69));
+		this.drawBloque(g2d, 960, 385, 50, new Color(246, 216, 46), new Color(223, 162, 69));
+		g2d.setColor(Color.BLACK);
+		g2d.fillOval(915, 395, 8, 20);
+		g2d.fillOval(938, 395, 8, 20);
+		g2d.fillOval(975, 395, 8, 20);
+		
+		GradientPaint gradientePasto = new GradientPaint(0, 600, new Color(19, 196, 18), 0, 620, new Color(30, 119, 56));
+		g2d.setPaint(gradientePasto);
+		g2d.fillRect(0, 600, 1000, 20);
+		
+		GradientPaint gradienteSuelo = new GradientPaint(0, 620, new Color(128, 115, 42), 0, 800, new Color(206, 156, 99));
+		g2d.setPaint(gradienteSuelo);
+		g2d.fillRect(0, 620, 1000, 180);
+		
+		Image mario = new ImageIcon("img/mario1.png").getImage();
+		g2d.drawImage(mario, 360, 464, 90, 136, null);
+		
+		Image piranha = new ImageIcon("img/piranha.png").getImage();
+		g2d.drawImage(piranha, 475, 280, 100, 100, null);
+	}
+	
+	private void drawFondo(Graphics2D g2d, int x, int y, int width, int height, Color color, Color colorSombreado) {
+		float[] fracciones = {0.0f, 1.0f};
+		Color[] colores = {color, colorSombreado};
+		
+		LinearGradientPaint gradiente = new LinearGradientPaint(x, y, x+width, y, fracciones, colores);
+		g2d.setPaint(gradiente);
+		g2d.fillArc(x, y, width, 200, 0, 180);
+		g2d.fillRect(x, y+100, width, height-100);
+		g2d.setPaint(null);
+ 	}
+	
+	private void drawNube(Graphics2D g2d, int x, int y) {
+		float[] fracciones = {0.20f, 1.0f};
+		Color[] colores = {Color.WHITE, new Color(192, 222, 226)};
+		
+		LinearGradientPaint gradiente = new LinearGradientPaint(x, y, x+250, y+50, fracciones, colores);
+		g2d.setPaint(gradiente);
+		g2d.fillRoundRect(x, y, 250, 50, 120, 50);
+		g2d.setPaint(null);
+	}
+
+	private void drawTuberiaAzul(Graphics2D g2d, int x, int y) {
+		float[] fracciones = {0.10f, 0.30f, 0.40f, 0.50f, 0.70f, 1.0f};
+		Color[] colores = {new Color(80, 78, 137), Color.WHITE, new Color(134, 131, 205),
+							new Color(110, 106, 185), new Color(77, 78, 143), new Color(39, 40, 75)};
+		LinearGradientPaint gradienteTuberia = new LinearGradientPaint(x, y, x+100, y, fracciones, colores);
+		g2d.setPaint(gradienteTuberia);
+		
+		g2d.fillRect(x, y, 110, 50);
+		g2d.fillRect(x+5, y+50, 100, 120);
+		g2d.setPaint(null);
 	}
 }
